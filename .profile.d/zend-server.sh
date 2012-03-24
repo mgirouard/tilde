6dbf93eddedac802803f7dbe4d644d4eae09c412
Michael Girouard <mgirouard@gmail.com>
2012-03-24 14:29:46 -0400

    Added back in missing $MYSQL reference
    Maybe this should be in a separate mysql.sh?

--- .profile.d/zend-server.sh
+++ .profile.d/zend-server.sh
@@ -1,5 +1,8 @@
-alias zs='sudo zendctl.sh'
-
 export ZEND=/usr/local/zend
-export PATH=$ZEND/bin:$PATH
+export MYSQL=$ZEND/mysql
+
+export PATH=$ZEND/bin:$MYSQL/bin:$PATH
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$ZEND/bin
+
+alias zs='sudo zendctl.sh'
+alias zsfixmysql="sudo ln -s $ZEND/tmp/mysql.sock /tmp/mysql.sock"
