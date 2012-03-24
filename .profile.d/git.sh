f5990a50c6d1690fcfb6c07cc5e582024a9ada8f
Michael Girouard <michaelgirouard@m2-corp.com>
2012-03-24 13:57:00 -0400

    Adding Git stuff
    Currently, just bash completion. Need to add prompt hacks.

---
+++ .profile.d/git.sh
@@ -0,0 +1,7 @@
+# Bash completion for Git (Homebrew Style)
+# FIXME: Need a non-version specific way of handling this
+COMPLETION=/usr/local/Cellar/git/1.7.9.2/etc/bash_completion.d/git-completion.bash
+[[ -f $COMPLETION ]] && source $COMPLETION
+
+# Prompt Hacks
+# TODO
