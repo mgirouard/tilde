e69dcfa69a19cf68ae1585812c6e6b8d0f329648
Michael Girouard <michaelgirouard@m2-corp.com>
2012-03-26 13:20:55 -0400

    Added globals for $PROJECTS and $VENDORS

--- .profile.d/shortcuts.sh
+++ .profile.d/shortcuts.sh
@@ -8,5 +8,9 @@ alias ls='ls -G'
 alias ll='ls -la'
 alias vi=vim
 
+# Common Paths
+export PROJECTS=$HOME/Documents/Projects
+export VENDORS=$HOME/Documents/Vendors
+
 # User Scripts
 export PATH=$HOME/bin:/usr/local/bin:$PATH
